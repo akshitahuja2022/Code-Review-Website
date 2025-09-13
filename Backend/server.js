e3d248bf5d18ecccec4a20a930454bbdfcd8a52d
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-09-13 12:40:39 +0530

    Intial File

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,21 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST"],
+  })
+);
 app.use("/ai", router);
 
 app.get("/", (req, res) => {
   res.send("Welcome to CodeReview Server");
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:4000");
 });
