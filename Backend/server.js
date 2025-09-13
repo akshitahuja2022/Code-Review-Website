9d0778876596d8b08c2dc88a5fdbd81acde136c8
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-09-13 15:36:21 +0530

    Initial File

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors());
 app.use("/ai", router);
 
 app.get("/", (req, res) => {
